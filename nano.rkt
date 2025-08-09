5cec1ba03d8bc747f4b8e36b413caafc34a4d30e
NoahStoryM <NoahStoryM@gmail.com>
2025-08-09 13:39:09 +0800

    Update

--- nano.rkt
+++ nano.rkt
@@ -2,30 +2,18 @@
 
 (provide (all-defined-out))
 
-(define-type Term (∪ Boolean Complex Char Bytes String Keyword Null Symbol Var (Pair Term Term)))
-(define-type Substitution (Immutable-HashTable Var Term))
-(define-type Goal (→ State State))
+(define-type Term
+  (∪ Boolean Complex Char Bytes String Keyword Null Symbol
+     Var (Pair Term Term)))
 
 (struct var
   ([counter : Natural])
   #:type-name Var
   #:transparent)
-
-(struct state
-  ([substitution : Substitution]
-   [counter : Natural])
-  #:type-name State
-  #:transparent)
-
-
-(: apply-goal (→ Goal State State))
-(define (apply-goal g s/c) (g s/c))
-
-
 (: var=? (→ Var Var Boolean))
 (define (var=? x1 x2) (= (var-counter x1) (var-counter x2)))
 
-
+(define-type Substitution (Immutable-HashTable Var Term))
 (: empty-s Substitution)
 (: ext-s (→ Var Term Substitution Substitution))
 (: size-s (→ Substitution Index))
@@ -36,3 +24,13 @@
 (define (size-s s) (hash-count s))
 (define (in-s? v s) (hash-has-key? s v))
 (define (apply-s s v) (hash-ref s v))
+
+(struct state
+  ([substitution : Substitution]
+   [counter : Natural])
+  #:type-name State
+  #:transparent)
+
+(define-type Goal (→ State State))
+(: apply-goal (→ Goal State State))
+(define (apply-goal g s/c) (g s/c))
