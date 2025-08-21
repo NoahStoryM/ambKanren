6192166872c0d532c4b5e1676b66c92d662952c8
NoahStoryM <NoahStoryM@gmail.com>
2025-08-21 17:31:54 +0800

    [demo] fix `occurs?`

--- demo.rkt
+++ demo.rkt
@@ -51,7 +51,7 @@
 (define (occurs? x v s)
   (let ([v (walk v s)])
     (or (and (var? v)
-             (eqv? x v))
+             (var=? x v))
         (and (pair? v)
              (or (occurs? x (car v) s)
                  (occurs? x (cdr v) s))))))
